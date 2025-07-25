c7dafe9b850b7234d8e9f0c667ce893871800300
daniel.stokes <danielphilipstokes@gmail.com>
2025-07-25 08:36:04 -0400

    github pages docs build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [
+		vue(),
+	],
+	base: '/vue-weather-radar/',
+	build: {
+		outDir: 'docs',
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
